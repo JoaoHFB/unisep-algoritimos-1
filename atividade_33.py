f9b341bd2d1a50833e7f67d9b9a9e6b1f43963ea
JoaoHFB <ujoaozinho121@gmail.com>
2024-03-30 20:26:26 -0300

    33-35

---
+++ atividade_33.py
@@ -0,0 +1,5 @@
+altura_degrau = float(input("Digite a altura do degrau: "))
+altura_deseja_alcançar = float(input("Digite a altura que deseja subir: "))
+altura_desejada = altura_degrau / altura_deseja_alcançar
+
+print("A quantidade de degraus até o final é: ", altura_desejada)
---
+++ atividade_34.py
@@ -0,0 +1,3 @@
+letra_maiuscula = input("digit uma letra maiuscula: ")
+letra_minuscula = chr(ord(letra_maiuscula) + 32)
+print("A letra minuscula correspondente é: ", letra_minuscula)
